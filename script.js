c4aacf5a58b0fdbe78cac4a159a78df05c4c6a79
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-06-19 11:37:04 -0400

    Added Magic Conch section

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function display(x){
     title.textContent = 'Your opponent chose ' + x +'! ';
     title.style.marginLeft = '15px';
     title.style.fontSize = '20px'
+    title.style.fontFamily = "'Cinzel', serif";
+    title.style.fontWeight = 'bold';
     document.getElementById('opponent').appendChild(title);
     
     }
@@ -24,7 +26,7 @@ function rock(){
         result = 'YOU LOSE'
     } else {
         result = 'IT\'S A TIE'
-    }
+    }    
     display(a)
     return result
 }
@@ -55,4 +57,12 @@ function scissors(){
     }
     display(a)
     return result
+}
+
+
+let ballOptions = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it", "Yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy", "try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+function magicBall(){
+    let x =  Math.floor(Math.random()*22);     
+    return ballOptions[x]
 }
